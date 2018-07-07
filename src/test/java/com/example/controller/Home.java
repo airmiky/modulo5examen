d56d86f74d1fb1102681e6ed08cc3d9a280bc544
Miguel Rojas <airmiky@gmail.com>
2018-07-06 21:12:39 -0400

    firts structure project

---
+++ src/test/java/com/example/controller/Home.java
@@ -0,0 +1,21 @@
+/**
+ * @author Miguel Rojas
+ */
+package com.example.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+
+    @RequestMapping(
+            value = "/demo/home",
+            method = RequestMethod.GET
+    )
+    public String holaMundo() {
+        return "Hola Mundo";
+    }
+}
