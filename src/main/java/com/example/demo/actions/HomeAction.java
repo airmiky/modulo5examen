cbf64624795cf4b2455d2f1945cf899064b426ce
Miguel Rojas <airmiky@gmail.com>
2018-07-06 21:25:41 -0400

    add action to home

---
+++ src/main/java/com/example/demo/actions/HomeAction.java
@@ -0,0 +1,10 @@
+/**
+ * @author Miguel Rojas
+ */
+package com.example.demo.actions;
+
+public class HomeAction {
+    public void someAction() {
+        System.out.println("Action in home");
+    }
+}
